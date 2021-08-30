5b905f6839fc7d3f0398b2c83f10ad610f44e299
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-08-30 22:31:08 +0300

    Added Functions
    I didn't realize the instructions said to use functions.

--- arrayOfGrades.js
+++ arrayOfGrades.js
@@ -2,17 +2,24 @@
 const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
 // your code goes here
 //The range of the second half year grades
-const halfYearGrades = grades.slice(grades.length / 2);
-halfYearGrades.sort(function (a, b) {return a - b;});
-const halfYearRange =
-  halfYearGrades[halfYearGrades.length - 1] - halfYearGrades[0];
+const halfYearRange = halfYearRangeFunction();
+function halfYearRangeFunction() {
+  const halfYearGrades = grades.slice(grades.length / 2);
+  halfYearGrades.sort((a, b) => a - b);
+  return halfYearGrades[halfYearGrades.length - 1] - halfYearGrades[0];
+}
 //The range of all the grades
-grades.sort(function (a, b) {return a - b;});
-const range = grades[grades.length - 1] - grades[0];
+const range = rangeFunction();
+function rangeFunction() {
+  grades.sort((a, b) => a - b);
+  return grades[grades.length - 1] - grades[0];
+}
 //median
-let median;
-if (grades.length % 2 === 1) median = grades[grades.length / 2 - 0.5];
-else median = (grades[grades.length / 2] + grades[grades.length / 2 - 1]) / 2;
+const median = medianFunction();
+function medianFunction() {
+  if (grades.length % 2 === 1) return grades[grades.length / 2 - 0.5];
+  return (grades[grades.length / 2] + grades[grades.length / 2 - 1]) / 2;
+}
 
 console.log(
   'Stats:\n\tRange: ' +
