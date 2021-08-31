31cd80aa00424bec36b098eae04a6a49a43568e0
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-09-01 02:15:30 +0300

    Object Exercise
    I did the objects exercise by saying that if the name of the driver isn't already a property in the array add it and initialize the property value to one. As the 'for each' continues to iterate through the array, every time it sees the drivers name add one to the existing value at that property in the object.

---
+++ objectExercise.js
@@ -0,0 +1,34 @@
+'use strict'
+const formula1Champions = [
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Alonso",
+    "Alonso",
+    "Räikkönen",
+    "Hamilton",
+    "Button",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Hamilton",
+    "Hamilton",
+    "Rosberg",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton"
+]
+let winners = {}
+
+function countWin(driver) {
+    // your code goes here
+        if (!(winners.hasOwnProperty(driver))) winners[driver] = 1;
+        else winners[driver]+=1;
+}
+formula1Champions.forEach(countWin);
+
+
